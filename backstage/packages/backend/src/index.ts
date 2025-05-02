b2cbc6149fb1b636548b0e6fcad774cd9e15c4c8
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2025-05-02 20:20:04 +0200

    feat: add github org plugin
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -20,6 +20,9 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
+backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
+
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
